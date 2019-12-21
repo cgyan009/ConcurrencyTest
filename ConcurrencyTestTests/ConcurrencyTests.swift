c5016f8eeb8b8e00e48a03e9afdbdcc4d761b161
Frank Yan <frank.v.yan@gmail.com>
2019-12-20 19:00:01 -0700

    Implement unit test verify the successful loading

--- ConcurrencyTestTests/ConcurrencyTests.swift
+++ ConcurrencyTestTests/ConcurrencyTests.swift
@@ -8,8 +8,20 @@ import XCTest
 @testable import ConcurrencyTest
 
 class ConcurrencyTests: XCTestCase {
-    func testloadMessage() {
-        
-    }
+    
+    let successfulGreetingMessage = "Hello world!"
+     
+     func testloadMessageSuccessfully() {
+         let expectation = XCTestExpectation(description: "Load message")
+         //make sure timeout is longer than maxium loading time
+         let loadingTimeout = DispatchTimeInterval.milliseconds(3000)
+         
+         loadGreetingMessage(timeout: loadingTimeout) { (message) in
+             XCTAssertNotNil(message)
+             XCTAssert(message == self.successfulGreetingMessage)
+             expectation.fulfill()
+         }
+         wait(for: [expectation], timeout: 10.0)
+     }
 
 }
